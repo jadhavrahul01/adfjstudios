8c6749e73d1e3d11f2eb2cf6b119ba890694a8c0
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-07-26 09:32:56 +0530

    small changes

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -177,3 +177,91 @@
       </div>
     </header>
     <!-- header area end -->
+
+        <!-- offcanvas area start -->
+        <div class="offcanvas__area offcanvas__area-4">
+            <div class="offcanvas__wrapper">
+              <div class="offcanvas__close">
+                <button
+                  class="offcanvas__close-btn offcanvas__close-btn-4 offcanvas-close-btn"
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M17 1L1 17"
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M1 1L17 17"
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div class="offcanvas__content">
+                <div
+                  class="offcanvas__top mb-10 d-flex justify-content-between align-items-center"
+                >
+                  <div class="offcanvas__logo logo">
+                    <a href="home-main.html">
+                      <img
+                        src="{{url('frontend/img/logo/adfjlight.svg')}}"
+                        alt="logo"
+                        height="75px"
+                        width="150px"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div class="offcanvas__inner d-none d-lg-block">
+                  <h4>We help to create visual strategies.</h4>
+                  <p>We want to hear from you. Let us know how we can help.</p>
+                  <img src="assets/img/shape/offcanvas-img-1.png" alt="" />
+                </div>
+                <div class="mobile-menu fix mb-40"></div>
+
+                <div class="offcanvas__inner-btn mb-80">
+                  <a href="get-started.html" class="tp-btn-offcanvas-white w-100"
+                    >Get Started</a
+                  >
+                </div>
+
+                <div class="offcanvas__contact mb-40">
+                  <p class="offcanvas__contact-call">
+                    <a href="tel:+918390194975">+91 8390194975</a>
+                  </p>
+                  <p class="offcanvas__contact-mail">
+                    <a href="mailto:hello@adfjstudios.com">hello@adfjstudios.com</a>
+                  </p>
+                </div>
+
+                <div class="offcanvas__social-4">
+                  <a href="https://www.facebook.com/adfjstudios"
+                    ><i class="fa-brands fa-facebook-f"></i
+                  ></a>
+                  <a href="https://twitter.com/adfjstudios?lang=en"
+                    ><i class="fa-brands fa-twitter"></i
+                  ></a>
+                  <a href="https://www.instagram.com/adfjstudios/"
+                    ><i class="fa-brands fa-instagram"></i
+                  ></a>
+                  <a href="https://www.youtube.com/@AdFJStudios"
+                    ><i class="fa-brands fa-youtube"></i
+                  ></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="body-overlay"></div>
+          <!-- offcanvas area end -->
